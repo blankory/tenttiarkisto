f3dc528a5a8f69f48ed1cecf6bf51957eef16dcf
Jesse Korhonen <jesse.korhonen@student.oulu.fi>
2018-04-13 08:24:30 +0300

    Added support for material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material/material.module';
+
 
 
 @NgModule({
@@ -11,7 +13,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
     AppRoutingModule
   ],
   providers: [],
